e521a28a949a195a1f58c521a37c041a3f5aa0f0
vsvami <106614918+vsvami@users.noreply.github.com>
2024-03-25 13:23:20 +0400

    add episodesViewController

---
+++ RickAndMorty/ViewControllers/EpisodesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EpisodesViewController.swift
+//  RickAndMorty
+//
+//  Created by Vladimir Dmitriev on 25.03.24.
+//
+
+import UIKit
+
+final class EpisodesViewController: UIViewController {
+
+    // MARK: - Public properties
+    var character: Character!
+    
+    // MARK: - Private Properties
+    private let networkManager = NetworkManager.shared
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
